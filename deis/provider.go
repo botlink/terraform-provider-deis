dd3aacee9933f80a42ebc19e16be5758405bd32f
Matthew Sullivan <matthew@msull92.com>
2015-12-11 23:38:00 -0600

    init

---
+++ deis/provider.go
@@ -0,0 +1,11 @@
+package datadog
+
+import "github.com/hashicorp/terraform/helper/schema"
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() {
+}
+
+// ProviderConfigure returns a configured client.
+func providerConfigure(d *schema.ResourceData) {
+}
